dfe245793fad6f00eb2d8221b7145e64a2f870cb
kindesskay <kindenessudubamo@gmail.com>
2024-01-30 22:38:13 +0100

    Adding questions and routes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const questions = require("./questions.json");
-const questions2=require('./questions2.json')
+const questions2 = require("./questions2.json");
 const app = express();
 const port = 3000;
 app.use(cors());
@@ -29,28 +29,28 @@ app.post("/answer/:id", (req, res) => {
     return res.status(404).json({ error: "Question not found" });
   }
 
-  const correctAnswer = questions[questionID].correct_option;  
-  const correctAnswer_string=questions[questionID].options[correctAnswer];
+  const correctAnswer = questions[questionID].correct_option;
+  const correctAnswer_string = questions[questionID].options[correctAnswer];
 
-  if (userAnswer==correctAnswer_string) {
+  if (userAnswer == correctAnswer_string) {
     res.json({ correct: true, message: "Correct answer" });
   } else {
     res.json({ correct: false, message: "Wrong answer" });
   }
-  
 });
 
-app.get('/question2/:id',(req,res)=>{
-  const question2ID=parseInt(req.params.id)
-  if(!question2ID|| question2ID <0 || !questions2[question2ID]){
-    return res.status(404).json({error:"Question not found"})
+app.get("/question2/:id", (req, res) => {
+  const questionID = parseInt(req.params.id);
+  if (!questionID || questionID < 0 || !questions2[questionID]) {
+    return res.status(404).json({ error: "Question not found" });
   }
-  return res.status(200).json({message:"Under construction"})
-
-})
+  const { question, options, id } = questions2[questionID];
+  const questionWithoutAnswer = { question, options, id };
+  return res.json(questionWithoutAnswer);
+});
 
 app.listen(port, () => {
   console.log(`server is  running on ${port}`);
 });
 
-module.exports=app
+module.exports = app;
